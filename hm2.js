33a3fb745fb1349bf2d5db2aeba927bdff353e0f
petrovnikola <115044256+petrovnikola@users.noreply.github.com>
2022-11-19 16:26:41 +0100

    Add files via upload

---
+++ hm2.js
@@ -0,0 +1,35 @@
+//hw1
+var favoriteRecipe = {
+    title:  "Macedonian Chopped Salad",
+    servings: "Servers:1",
+    ingredients: ["tomatoes" , "cucumbers" , "onions" , "peppers " ]
+};
+
+    console.log(favoriteRecipe);  
+    console.log(favoriteRecipe.title);
+    console.log(favoriteRecipe.servings);
+    
+    
+    var ingredients = [
+        "tomatoes" , "cucumbers" , "onions" , "peppers " 
+    ];
+    console.log(ingredients[0]);
+    console.log(ingredients[1]);
+    console.log(ingredients[2]);
+    console.log(ingredients[3]);
+
+
+    //hw2
+
+    var programming_languages = ["Python" , "Java" , "C++" , "#C" , "JavaScript" , "SQL" , "PHP" , "GO"];
+    console.log(programming_languages);
+    console.log(programming_languages.length);
+    var removelast = programming_languages.pop();
+    console.log(programming_languages);
+    var new_lang = programming_languages.splice(2,1, "Kotlin");
+    console.log(programming_languages);
+
+
+
+    
+    
